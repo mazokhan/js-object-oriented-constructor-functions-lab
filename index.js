66c16a752934d58b0d34b9ac87c660800172a0f3
mazen <mak9986@hotmail.com>
2021-11-09 21:07:40 +1100

    complete

---
+++ index.js
@@ -0,0 +1,17 @@
+function(year,color,model){
+    this.year = year;
+    this.color = color;
+    this.model = model
+}
+
+function Driver(name,age,experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+function pickuplocation(address, city){
+    this.address = address;
+    this.city = city;
+}
+
